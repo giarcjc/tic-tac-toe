208a51bdff9c7ffd7d72b633480609d7cbf95fea
chriscraig <chris@cjcraig.com>
2018-06-15 23:44:57 -0600

    add calculateWinner function so game can end

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squareArray.slice(); // .slice to copy squares array instead of mutating
+    if (calculateWinner(squares)) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squareArray: squares,
@@ -37,7 +41,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next Player X';
+    const winner = calculateWinner(this.state.squareArray);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -84,3 +96,24 @@ class Game extends React.Component {
 }
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
